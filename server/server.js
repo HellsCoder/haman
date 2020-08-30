4f501ea08a2718d3b1eb386791d1e93432fee352
HellsCoder <hellscoder@yandex.ru>
2020-08-30 23:38:58 +0200

    change express

--- server/server.js
+++ server/server.js
@@ -2,10 +2,6 @@ const app = require('express')();
 
 /*
     Haman - библиотека для лонг-пул реал-тайм соединений. Встраивается в любое express приложение
-
-    haman.send(to, event, data) - отправить данные конкретном клиенту, где to - его ключ
-    haman.broadcast(from, event, data) - отправить данные всем клиентам кроме from, from - тот
-    от кого данные пришли, оставьте null если данные идут от сервера
 */
 const haman = require('./haman/haman')(app);
 
