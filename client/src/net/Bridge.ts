c7ef171477c4131a98f7850e09192296fb36400b
HellsCoder <hellscoder@yandex.ru>
2020-09-08 13:46:45 +0200

    new logger

--- client/src/net/Bridge.ts
+++ client/src/net/Bridge.ts
@@ -1,15 +1,18 @@
 import Net from './Net';
 import EventBus from '../event/EventBus';
+import Haman from './../Haman';
 
 
 export default class Bridge {
 
     private host: string;
     private key : string;
+    private haman : Haman;
 
-    constructor(host : string, key : string){
+    constructor(host : string, key : string, haman : Haman){
         this.host = host;
         this.key = key;
+        this.haman = haman;
     }
 
     public connectWhile(eventBus: EventBus){
@@ -17,6 +20,12 @@ export default class Bridge {
             let json = JSON.parse(decodeURIComponent(atob(data))); //{ts: 99999999, d: {e: 'eventName', dt: 'data'}}
             if(!json.d){
                 return this.connectWhile(eventBus);
+            }
+            if(this.haman.isLogging()){
+                console.group('PacketRecive');
+                console.info(json.d.e);
+                console.info(json.d.dt);
+                console.groupEnd();
             }
             eventBus.invokeEvent(json.d.e, json.d.dt);
             return this.connectWhile(eventBus);
